cc6c82b5fa246477d7e2d032ea4667bba0cf7858
Patricio Marrone <marrone.patricio.test@gmail.com>
2020-12-18 02:31:48 +0100

    Adding test for invalid digits

--- romanNumeralTranslator.test.js
+++ romanNumeralTranslator.test.js
@@ -1,6 +1,12 @@
 const romanNumeralTranslator = require('./romanNumeralTranslator.js')
 
-test('roman numerals', () => {
-  expect(romanNumeralTranslator('MCMLIV')).toBe(1954)
-  expect(romanNumeralTranslator('MCCLXXVIII')).toBe(1278)
+describe('romanNumeralTranslator', () => {
+  it('should successfully handle example cases', () => {
+    expect(romanNumeralTranslator('MCMLIV')).toBe(1954)
+    expect(romanNumeralTranslator('MCCLXXVIII')).toBe(1278)
+  })
+
+  it('should throw an error when the passed value has some non-roman digits', () => {
+    expect(() => romanNumeralTranslator('MCMDLIG')).toThrow()
+  })
 })
