81cebbadc7960b2bda0adbc33369326211e42d23
Patricio Marrone <marrone.patricio.test@gmail.com>
2020-12-18 01:12:49 +0100

    Refactoring tests scripts

--- index.js
+++ index.js
@@ -2,12 +2,7 @@ const os = require("os");
 const http = require("http");
 const url = require("url");
 
-// Write a function that takes a roman numeral as input,
-// and returns the number as an integer
-// Don't remember roman numerals? Check https://www.mathsisfun.com/roman-numerals.html
-const romanNumeralTranslator = (str) => {
-  return 'Test'
-}  // code goes here;
+const romanNumeralTranslator = require("./romanNumeralTranslator.js");
 
 const handleRequest = (req, res) => {
   const { roman } = url.parse(req.url, true).query;
@@ -19,5 +14,3 @@ const handleRequest = (req, res) => {
 };
 
 http.createServer(handleRequest).listen(3000);
-
-module.exports = romanNumeralTranslator;
---
+++ romanNumeralTranslator.js
@@ -0,0 +1,8 @@
+// Write a function that takes a roman numeral as input,
+// and returns the number as an integer
+// Don't remember roman numerals? Check https://www.mathsisfun.com/roman-numerals.html
+const romanNumeralTranslator = (str) => {
+  return 'Test'
+}  // code goes here;
+
+module.exports = romanNumeralTranslator
